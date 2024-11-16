939bae44f4bcf13504afa3809c4585c401f0beeb
ShuXuanyu <156899457+QianYUHA@users.noreply.github.com>
2024-11-16 23:17:45 +0800

    Add files via upload

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"AppointmentManagement"},{"l":"HospitalManagement.Testing"},{"l":"InventoryManagement"},{"l":"MedicalRecords"},{"l":"UserManagment"}];updateSearchResults();
