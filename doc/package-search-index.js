f6e6f09e232df3fe82c0d34fa2e5f555a6de49de
ShuXuanyu <156899457+QianYUHA@users.noreply.github.com>
2024-11-17 20:02:58 +0800

    Delete doc directory

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"hospital.appointmentmanagement"},{"l":"hospital.inventorymanagement"},{"l":"hospital.mainapp"},{"l":"hospital.medicalrecords"},{"l":"hospital.usermanagment"}];updateSearchResults();
